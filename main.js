afe8a667cbd9c76fa9368c08893936b6e0d69a19
Drishti Wahi <69460490+drishtiwahi@users.noreply.github.com>
2022-03-31 23:57:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+document.getElementById("count-btn").onclick = function () {
+  var total_length = document.getElementById("char-text").value.length;
+  document.getElementById("char-length").innerText = total_length;
+}
+
